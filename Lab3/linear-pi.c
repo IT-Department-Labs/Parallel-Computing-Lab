15e1e8513498add94535400d0c194959bcae3e24
Salman Shah <sbs.191197@gmail.com>
2017-08-21 11:55:51 +0530

    New Lab 3 code added

---
+++ Lab3/linear-pi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+	double pi,x;
+	int i, N;
+	pi = 0.0;
+	N = 1000;
+	for(i = 0; i <= N; i++) {
+		x =(double)i/N;
+		pi += 4/(1+x*x);
+	}
+
+	pi = pi/N;
+	printf("%f\n",pi);
+
+	return 0;
+}
